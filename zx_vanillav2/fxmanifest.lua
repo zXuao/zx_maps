6b16f66edae80862f64d71a1b6f59fac852df1ea
zXuao <77466114+zXuao@users.noreply.github.com>
2021-01-16 00:38:15 -0300

    first commit

---
+++ zx_vanillav2/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "adamant"
+
+game "gta5"
+
+this_is_a_map "yes"
+
+data_file "DLC_ITYP_REQUEST" "Stream/vw_prop_vw_accs_01.ytyp"
+data_file "DLC_ITYP_REQUEST" "Stream/ch_prop_ch_casino_backarea.ytyp"
+data_file "DLC_ITYP_REQUEST" "Stream/ch_prop_ch_casino_main.ytyp"
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+
+files {
+    "interiorproxies.meta"
+}
